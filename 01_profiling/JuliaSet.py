14818fc2b2a6df4fe02120bcb89405e87da07f49
Paul Mayer <p@mayer-zuffenhausen.de>
2025-01-20 12:15:06 +0100

    [1.1-2.1] intermediate results

--- 01_profiling/JuliaSet.py
+++ 01_profiling/JuliaSet.py
@@ -22,8 +22,63 @@ def timefn(fn):
     return measure_time
 
 
-@timefn
+# @profile
 def calc_pure_python(desired_width, max_iterations):
+    """Create a list of complex coordinates (zs) and complex parameters (cs),
+    build Julia set"""
+    x_step = (x2 - x1) / desired_width
+    y_step = (y1 - y2) / desired_width
+    x = []
+    y = []
+    ycoord = y2
+    while ycoord > y1:
+        y.append(ycoord)
+        ycoord += y_step
+    xcoord = x1
+    while xcoord < x2:
+        x.append(xcoord)
+        xcoord += x_step
+    # build a list of coordinates and the initial condition for each cell.
+    # Note that our initial condition is a constant and could easily be removed,
+    # we use it to simulate a real-world scenario with several inputs to our
+    # function
+    zs = []
+    cs = []
+    for ycoord in y:
+        for xcoord in x:
+            zs.append(complex(xcoord, ycoord))
+            cs.append(complex(c_real, c_imag))
+
+    print("Length of x:", len(x))
+    print("Total elements:", len(zs))
+    output = calculate_z_serial_purepython(max_iterations, zs, cs)
+    print(f"width:\t{desired_width}\noutput:\t{sum(output)}")
+
+    if desired_width == 1000:
+        # This sum is expected for a 1000^2 grid with 300 iterations
+        assert sum(output) == 33219980
+    elif desired_width == 10000:
+        assert sum(output) == 3323787446
+    else:
+        print("no asserts for this dimension...")
+
+
+# @profile
+def calculate_z_serial_purepython(maxiter, zs, cs):
+    """Calculate output list using Julia update rule"""
+    output = [0] * len(zs)
+    for i in range(len(zs)):
+        n = 0
+        z = zs[i]
+        c = cs[i]
+        while abs(z) < 2 and n < maxiter:
+            z = z * z + c
+            n += 1
+        output[i] = n
+    return output
+
+@timefn
+def calc_pure_python_profiled(desired_width, max_iterations):
     """Create a list of complex coordinates (zs) and complex parameters (cs),
     build Julia set"""
     x_step = (x2 - x1) / desired_width
@@ -51,7 +106,7 @@ def calc_pure_python(desired_width, max_iterations):
 
     #print("Length of x:", len(x))
     #print("Total elements:", len(zs))
-    output, t = calculate_z_serial_purepython(max_iterations, zs, cs)
+    output, t = calculate_z_serial_purepython_profiled(max_iterations, zs, cs)
     # print(f"width:\t{desired_width}\noutput:\t{sum(output)}")
 
     if desired_width == 1000:
@@ -66,7 +121,7 @@ def calc_pure_python(desired_width, max_iterations):
 
 
 @timefn
-def calculate_z_serial_purepython(maxiter, zs, cs):
+def calculate_z_serial_purepython_profiled(maxiter, zs, cs):
     """Calculate output list using Julia update rule"""
     output = [0] * len(zs)
     for i in range(len(zs)):
@@ -80,10 +135,7 @@ def calculate_z_serial_purepython(maxiter, zs, cs):
     return output
 
 
-if __name__ == "__main__":
-    # Calculate the Julia set using a pure Python solution with
-    # reasonable defaults for a laptop
-
+def do_profiling():
     n = 20
     desired_width = 1000
     max_iterations = 300
@@ -91,10 +143,22 @@ if __name__ == "__main__":
     t_outer = np.zeros(n)
 
     for i in range(n):
-        t1, t2 = calc_pure_python(desired_width=1000, max_iterations=300)
+        t1, t2 = calc_pure_python_profiled(desired_width=desired_width, max_iterations=max_iterations)
         t_inner[i] = t2
         t_outer[i] = t1
 
     print(f"number of runs: {n}\ndesired width: {desired_width}\nmax iterations: {max_iterations}\n\n" + \
           f"calc_pure_python\n\tmean: {np.mean(t_outer)} s\n\tstd: {np.std(t_outer, dtype=np.float64)} s\n\n" + \
           f"calculate_z_serial_purepython\n\tmean: {np.mean(t_inner)} s\n\tstd: {np.std(t_inner, dtype=np.float64)} s")
+
+
+
+if __name__ == "__main__":
+    # Calculate the Julia set using a pure Python solution with
+    # reasonable defaults for a laptop
+
+    # do_profiling()
+    t1 = timer()
+    calc_pure_python(desired_width=100, max_iterations=300)
+    t2 = timer()
+    print(f"total calculation time: {t2 - t1} s")
