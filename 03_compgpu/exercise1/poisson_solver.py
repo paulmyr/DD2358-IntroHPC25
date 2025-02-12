8efc3e6465cac2aa28df5c882747a54103ee5f2a
Rishi Vijayvargiya <rishivijayv@gmail.com>
2025-02-12 15:03:02 +0100

    feat(a3): cleaning up ex1 notebook and profiling for cython/python

--- 03_compgpu/exercise1/poisson_solver.py
+++ 03_compgpu/exercise1/poisson_solver.py
@@ -4,6 +4,8 @@ from timeit import default_timer as timer
 
 from line_profiler import profile
 import purepythonpoisson
+import cythonpoisson
+import h5py
 
 # @profile
 def run_pure_numpy(N, num_iterations):
@@ -14,28 +16,63 @@ def run_pure_numpy(N, num_iterations):
     for _ in range(num_iterations):
         grid = purepythonpoisson.numpy_pure_gauss_seidel(grid)
 
+def solve_poisson(fn, N, num_iterations):
+    """
+    Initializes a NxN according to the constraints given and then returns the resulting
+    answer after calling the provided solving function on it.
+
+    N: The dimensions of the square grid
+    num_iterations: The number of iterations to run the gauss_seidel algorithm for
+    fn: The solving funciton to call on each iteration
+
+    The grid initialized has the same seed to ensure consistent initialization and results
+    """
+    np.random.seed(42)
+    grid = np.zeros((N, N))
+    grid[1:-1, 1:-1] = np.random.rand(N-2, N-2)
+
+    for _ in range(num_iterations):
+        grid = fn(grid)
+
+    return grid 
+
 
 def timed(f, *args, **kwargs):
     t0 = timer()
-    f(*args, **kwargs)
+    solve_poisson(f, *args, **kwargs)
     t1 = timer()
     return t1 - t0
 
 
-def run_function_as_experiment(f, num_iterations = 1000):
-    n = [2**i for i in range(7, 8)]
-    wtimes = [timed(f, i, num_iterations) for i in n]
+def run_function_as_experiment(f, num_iterations = 100):
+    grid_sizes = [2**i for i in range(6, 11)]
+
+    num_runs = 5
+    wtimes = np.zeros(len(grid_sizes))
+    for _ in range(num_runs):
+        wtimes += [timed(f, curr_size, num_iterations) for curr_size in grid_sizes]
+    wtimes /= num_runs 
 
     print(f"ran {f.__name__}")
-    print(f"each grid ran {num_iterations} iterations.")
-    for i, j in zip(n, wtimes):
-        print(f"Grid: ({i}, {i})\n\truntime: {j}s")
+    print(f"each grid ran {num_iterations} iterations. ({num_runs} runs)")
+    for i, j in zip(grid_sizes, wtimes):
+        print(f"Grid: ({i}, {i})\n\tavg runtime: {j}s")
 
-    plt.plot(n, wtimes, label=f"{f.__name__}")
+    plt.plot(grid_sizes, wtimes, label=f"{f.__name__}", marker='o')
+
+def save_1024_grid_hdf5():
+    pure_python_result = solve_poisson(1024, 50, purepythonpoisson.numpy_pure_gauss_seidel)
+    cython_result = solve_poisson(1024, 50, cythonpoisson.numpy_cython_gauss_seidel)
+
+    f = h5py.File("matrix_python_cython.hdf5", "w")
+    f["/1024/pure_python"] = pure_python_result
+    f["/1024/cython"] = cython_result
 
 
 if __name__ == '__main__':
-    run_function_as_experiment(run_pure_numpy)
+    run_function_as_experiment(purepythonpoisson.numpy_pure_gauss_seidel)
+    run_function_as_experiment(cythonpoisson.numpy_cython_gauss_seidel)
+
 
     plt.yscale("log")
     plt.xscale("log")
@@ -44,3 +81,4 @@ if __name__ == '__main__':
     plt.legend()
 
     plt.show()
+    # save_1024_grid_hdf5()
--- 03_compgpu/exercise1/test_cython_purepython.py
+++ 03_compgpu/exercise1/test_cython_purepython.py
@@ -2,35 +2,16 @@ import pytest
 import numpy as np
 import purepythonpoisson
 import cythonpoisson
+from poisson_solver import solve_poisson
 
 # We fix the number of iterations here to prevent long test times.
 NUM_ITERATIONS = 50
 
-def solve_poisson(N, num_iterations, fn):
-    """
-    Initializes a NxN according to the constraints given and then returns the resulting
-    answer after calling the provided solving function on it.
-
-    N: The dimensions of the square grid
-    num_iterations: The number of iterations to run the gauss_seidel algorithm for
-    fn: The solving funciton to call on each iteration
-
-    The grid initialized has the same seed to ensure consistent initialization and results
-    """
-    np.random.seed(42)
-    grid = np.zeros((N, N))
-    grid[1:-1, 1:-1] = np.random.rand(N-2, N-2)
-
-    for _ in range(num_iterations):
-        grid = fn(grid)
-
-    return grid
-
 @pytest.mark.parametrize("grid_size", [64, 126, 256, 512, 1024])
 def test_mandelbrot_grid_diff_sizes(grid_size):
 
-    default_grid = solve_poisson(grid_size, NUM_ITERATIONS, purepythonpoisson.numpy_pure_gauss_seidel)
-    cython_grid = solve_poisson(grid_size, NUM_ITERATIONS, cythonpoisson.numpy_cython_gauss_seidel)
+    default_grid = solve_poisson(purepythonpoisson.numpy_pure_gauss_seidel, grid_size, NUM_ITERATIONS)
+    cython_grid = solve_poisson(cythonpoisson.numpy_cython_gauss_seidel, grid_size, NUM_ITERATIONS)
     
     assert np.array_equal(cython_grid, default_grid)
 
